ff1ef666c71df6863d668ac2febc6dccf022dc9c
ernestopcaraujo@gmail.com <ernestopcaraujo@gmail.com>
2023-03-27 15:07:58 -0300

    feat: configurada a rota de visualização dos posts

--- alkabot-blog/src/app/app-routing.module.ts
+++ alkabot-blog/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ViewpostsComponent } from './components/viewposts/viewposts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:ViewpostsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
